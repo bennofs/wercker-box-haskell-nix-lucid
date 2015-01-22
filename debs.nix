6a342e7342967cedb28a0742c1be18aea66c825a
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-01-22 17:10:24 +0100

    Initial commit

---
+++ debs.nix
@@ -0,0 +1,17 @@
+[
+{ url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/gcc-4.1-base_4.1.2-27ubuntu1_i386.deb"; sha256 = "84e64e0f4e356cb6d7bfb0ced80a4413c7ff87502980a3724d7de55abca6557b"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/cpp-4.1_4.1.2-27ubuntu1_i386.deb"; sha256 = "3ea4028a88c22eeee7357547b9cfd191d9f83d2e3a167daa6a155f3784ab2ac1"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/gcc-4.1_4.1.2-27ubuntu1_i386.deb"; sha256 = "88274de1899b68ccda9f114f423852a55d4c75bbb896ecabbdeaed2b069f8468"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/e/eglibc/libc-dev-bin_2.11.1-0ubuntu7.19_i386.deb"; sha256 = "ab542de4e3f9ecebf60bef5b76bb7dbc7b35c709ec853f992acfe1b9bc056025"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/l/linux/linux-libc-dev_2.6.32-71.138_i386.deb"; sha256 = "0bc8f8399dded9f12952fd6e7826b316dfccbc7960bb80480faed7baf46a27d0"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/e/eglibc/libc6-dev_2.11.1-0ubuntu7.19_i386.deb"; sha256 = "08374500025017ae608239d6ab87f596b502efcea16014654ebf56e68d264609"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/libstdc++6-4.1-dev_4.1.2-27ubuntu1_i386.deb"; sha256 = "0ccfbb616637fe20c7afa7ee8dbaec9e34f34d442935d2ccb5c6b847e3ec4754"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/g++-4.1_4.1.2-27ubuntu1_i386.deb"; sha256 = "1f9dc541d3accc7a3bba84ec1ef26af66ce45c126b4947cb30c83384b3023562"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-4.4/gcc-4.4_4.4.3-4ubuntu5.1_i386.deb"; sha256 = "fd91488cac764780779bec57b77309820400aa55e546f7cba7b907e0b97659b6"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-defaults/gcc_4.4.3-1ubuntu1_i386.deb"; sha256 = "59d1859ec638bb71df37871c3114af6b1433eb00fea82dbdef3f0809771e63cf"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmpxx4ldbl_4.3.2+dfsg-1ubuntu1_i386.deb"; sha256 = "7fa0dbc8031e7944a5120a97bbed9903c84e4b1d0f5c4b4ef04b01ef054e2516"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmp3-dev_4.3.2+dfsg-1ubuntu1_i386.deb"; sha256 = "1adb2b3abf160db8fb67f049a913cdc0ce1a1bd1768b8b560b501cc966623378"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/m/manpages/manpages-dev_3.23-1_all.deb"; sha256 = "0946993fcd2e5546bc617eb5a25281e9614103217773cc16caaff3eb1aa58332"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/p/pkg-config/pkg-config_0.22-1build2_i386.deb"; sha256 = "9cc055c96a53dc11e2d3ac6c22b99622cc0626c6d0ecf916827c741671eae204"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib1g-dev_1.2.3.3.dfsg-15ubuntu1_i386.deb"; sha256 = "e1041b3a4fff2c094f2364bc28d7624e50b4da38aab91f2a43f2e732ceb2b699"; }
+]
---
+++ lucid-haskell-packages.nix
@@ -0,0 +1,132 @@
+let
+  pkgs = import <nixpkgs> { config = {}; };
+  inherit (pkgs) haskellngPackages lib writeScript stdenv;
+  sources = {
+    ghc = rec {
+      src = pkgs.fetchurl {
+        url = "https://www.haskell.org/ghc/dist/${version}/ghc-${version}-i386-unknown-linux-centos65.tar.bz2";
+        sha256 = "082228lv4zf3jqxkibmjw3n1g54h5802r2jqa662afl0inm73v9g";
+      };
+      version = "7.8.4";
+    };
+
+    debs = rec {
+      srcs = map pkgs.fetchurl (import ./debs.nix);
+      src = pkgs.stdenv.mkDerivation {
+        name = "debs";
+        phases = ["buildPhase"];
+        buildPhase = with pkgs.lib; ''
+          mkdir $out;
+          ${concatStringsSep "\n" (map (x: "cp ${x} $out") srcs)}
+        '';
+      };
+    };
+  };
+
+  base-rootfs = with pkgs; with lib; stdenv.mkDerivation {
+    name = "sc-build-rootfs";
+    src = fetchurl {
+      url = "http://download.openvz.org/template/precreated/ubuntu-10.04-x86.tar.gz";
+      sha256 = "1zpir2a4w1m5ar82qc3zf6sj7xp6r883i76f8cl9xgzwc2nj4ds9";
+    };
+    buildInputs = [ proot ];
+    phases = [ "unpackPhase" "buildPhase" ];
+
+    binds = concatStringsSep " " (mapAttrsToList (n: v: "-b " + v.src + ":/sources/" + n) sources);
+    script = ''
+      export PATH=/bin:/sbin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+      ln -s /lib/libncurses.so.5 /usr/lib/libtinfo.so.5
+      ln -s /usr/lib/libtinfo.so.5 /usr/lib/libtinfo.so
+
+      echo "-- Installing dependencies ..." && dpkg -i /sources/debs/${"*"}
+      echo "-- Installing ghc ..." && cd /sources && tar -xf /sources/ghc && cd ghc-${sources.ghc.version} && ./configure && make install
+      cd /sources && rm -rf ghc-${sources.ghc.version}
+    '';
+
+    unpackPhase = ''
+      mkdir -p $out
+      cd $out
+      tar --exclude="./dev/*" --exclude="./lib/udev/devices/*" -xf $src
+      ln -s /proc/self/fd/ $out/lib/udev/devices/fd
+      ln -s /proc/kcore $out/lib/udev/devices/core
+      ln -s fd/0 $out/lib/udev/devices/stdin
+      ln -s fd/1 $out/lib/udev/devices/stdout
+      ln -s fd/2 $out/lib/udev/devices/stderr
+    '';
+
+    buildPhase = ''
+      echo "$script" > $out/provision
+      proot -w / $binds -b /proc -b /dev -0 -r $out /usr/bin/env -i /bin/bash /provision
+    '';
+  };
+
+  buildInChroot = deriv: lib.overrideDerivation deriv (old: {
+    args = ["-e"  (pkgs.writeScript "builder" '' exec \
+      ${pkgs.proot}/bin/proot -b /proc -b /nix -b /dev -b /tmp -0 -r ${base-rootfs} \
+        /usr/bin/env LANG=C /bin/bash --rcfile /etc/environment \
+          ${lib.concatStringsSep " " (map (x: "\"" + x + "\"") old.args)}
+      '')
+    ];
+  });
+
+  chrootGCC = lib.overrideDerivation
+    (buildInChroot (import <nixpkgs/pkgs/build-support/gcc-wrapper> {
+      nativeTools = true;
+      nativeLibc = true;
+      nativePrefix = "/usr";
+      shell = "/bin/bash";
+      name = "gcc-base-rootfs";
+      inherit stdenv;
+    })) (old: {
+      buildCommand = ''
+        ${old.buildCommand}
+        ln -s /usr/bin/ar $out/bin/ar
+      '';
+    });
+
+  chrootStdenv =
+    let env = stdenv.override (old: {
+      preHook = ''
+        ${old.preHook or ""}
+        export NIX_ENFORCE_PURITY=0
+      '';
+      cc = chrootGCC;
+      allowedRequisites = null;
+    }); in env // {
+      mkDerivation = attrs: buildInChroot (env.mkDerivation attrs);
+    };
+
+  chrootCabalBuilder =
+   import <nixpkgs/pkgs/development/haskell-modules/generic-builder.nix> {
+    inherit (pkgs) fetchurl glibcLocales coreutils gnugrep gnused;
+    stdenv = chrootStdenv;
+    inherit (haskellngPackages) jailbreak-cabal hscolour cpphs;
+    ghc = {
+      inherit (sources.ghc) version;
+      meta.platforms = lib.platforms.linux;
+      name = "ghc-${sources.ghc.version}";
+      outPath = "/usr/local";
+    };
+    pkgconfig = "/usr";
+  };
+
+  cabalDefaults = {
+    doCheck = false;
+    doHaddock = false;
+    enableLibraryProfiling = false;
+  };
+
+  chrootHaskellPackages = haskellngPackages.override (old: {
+    stdenv = chrootStdenv;
+    overrides = self: super: (old.overrides or (_: _: {})) self super // {
+      mkDerivation = args: chrootCabalBuilder (args // cabalDefaults);
+    };
+    pkgs = let result = pkgs // {
+      callPackage = pkgs.callPackageWithScope result;
+
+      # these are provided by the ubuntu base system
+      zlib = null;
+    }; in result;
+  });
+
+in chrootHaskellPackages
