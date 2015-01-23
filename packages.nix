1eb78a39b90e9e12bdd85484a78c12ef64019a00
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-01-23 13:51:28 +0100

    More packages

--- packages.nix
+++ packages.nix
@@ -1,32 +1,28 @@
-let
-  pkgs = import <nixpkgs> { config = {}; };
-  isEnabled = x: x == null || ! x ? pname ||
-    (if x ? enabled then x.enabled else builtins.throw "${x.pname} has no enabled");
-  haskellPackages = (import ./lucid-haskell-packages.nix).override (old: {
-    overrides = self: super:
-      let super = (old.overrides or (_: _: {})) self super;
-      in super // {
-        mkDerivation = args: super.mkDerivation (args // {
-          passthru = with pkgs.lib; with builtins; {
-            enabled =
-              let nativeDeps = args.extraLibraries or [] ++ args.pkgconfigDepends or [];
-              in all isEnabled (args.buildDepends or []) &&
-                 [] == filter (x: x != null) nativeDeps;
-          };
-        });
-      };
-  });
-  deriv = pkgs.stdenv.mkDerivation {
-    name = "stackage-packages";
-    buildCommand = ''
-      echo "{" >> $out
-      tr "\n" "," < ${./packages} >> $out
-      echo "}: { inherit " >> $out
-      cat ${./packages} >> $out
-      echo "; }" >> $out
-    '';
-  };
-  stackagePackages = with builtins; filter (x: !isFunction x && x != null) (attrValues (haskellPackages.callPackage (import deriv) {}));
-  extraPackages = with haskellPackages; [ storable-record lens conduit errors optparse-applicative ];
-  allPackages = with builtins; filter isEnabled extraPackages;
-in allPackages
+with (import ./haskell-packages.nix);
+builtins.filter (x: x != null) [
+async
+attoparsec
+blaze-builder
+blaze-html
+blaze-markup
+cereal
+conduit
+conduit-extra
+data-default
+deepseq
+deepseq-generics
+either
+errors
+lens
+mmorph
+monad-loops
+network
+optparse-applicative
+storable-record
+text
+th-lift
+th-lift-instances
+xml-conduit
+xml-types
+zlib
+]
