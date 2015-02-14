15ec3f777afc825e79699f66ae418cd09924218b
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2015-02-14 10:28:30 +0100

    update debs.nix

--- debs.nix
+++ debs.nix
@@ -1,8 +1,9 @@
 [
 # Generated using:
-# $ apt-get install --print-uris -yy -qq libgmp3-dev gcc pkg-config libz-dev | tr "'" '"' | cut -f1,4 -d' ' | sed -re 's/"([^"]*)" SHA256:(.*)$/{ url = "\1"; sha256 = "\2"; }/'
+# $ apt-get install --print-uris -yy -qq libssl-dev libgmp3-dev gcc pkg-config libz-dev | tr "'" '"' | cut -f1,4 -d' ' | sed -re 's/"([^"]*)" SHA256:(.*)$/{ url = "\1"; sha256 = "\2"; }/'
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/e/eglibc/libc-bin_2.11.1-0ubuntu7.20_i386.deb"; sha256 = "80f12fbe1d213512f20000dd3392a30880c09c378291bd573408b81a73668d8b"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/e/eglibc/libc6_2.11.1-0ubuntu7.20_i386.deb"; sha256 = "a424b1bdce0557aa9d1eee7033daad242082c7e0a44e03e7950003e6eb4caa22"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl0.9.8_0.9.8k-7ubuntu8.23_i386.deb"; sha256 = "b9852b42af4a509b552df21d4713e2456812c36e64a29d95eaac83b43cf3cc11"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/gcc-4.1-base_4.1.2-27ubuntu1_i386.deb"; sha256 = "84e64e0f4e356cb6d7bfb0ced80a4413c7ff87502980a3724d7de55abca6557b"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/cpp-4.1_4.1.2-27ubuntu1_i386.deb"; sha256 = "3ea4028a88c22eeee7357547b9cfd191d9f83d2e3a167daa6a155f3784ab2ac1"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/universe/g/gcc-4.1/gcc-4.1_4.1.2-27ubuntu1_i386.deb"; sha256 = "88274de1899b68ccda9f114f423852a55d4c75bbb896ecabbdeaed2b069f8468"; }
@@ -15,7 +16,8 @@
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gcc-defaults/gcc_4.4.3-1ubuntu1_i386.deb"; sha256 = "59d1859ec638bb71df37871c3114af6b1433eb00fea82dbdef3f0809771e63cf"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmpxx4ldbl_4.3.2+dfsg-1ubuntu1_i386.deb"; sha256 = "7fa0dbc8031e7944a5120a97bbed9903c84e4b1d0f5c4b4ef04b01ef054e2516"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/g/gmp/libgmp3-dev_4.3.2+dfsg-1ubuntu1_i386.deb"; sha256 = "1adb2b3abf160db8fb67f049a913cdc0ce1a1bd1768b8b560b501cc966623378"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib1g-dev_1.2.3.3.dfsg-15ubuntu1_i386.deb"; sha256 = "e1041b3a4fff2c094f2364bc28d7624e50b4da38aab91f2a43f2e732ceb2b699"; }
+{ url = "http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_0.9.8k-7ubuntu8.23_i386.deb"; sha256 = "0f80ee9a7253bcaf4402c7c6261abf6dc7f28cbae5527b13b3017d89b63cbd91"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/m/manpages/manpages-dev_3.23-1_all.deb"; sha256 = "0946993fcd2e5546bc617eb5a25281e9614103217773cc16caaff3eb1aa58332"; }
 { url = "http://archive.ubuntu.com/ubuntu/pool/main/p/pkg-config/pkg-config_0.22-1build2_i386.deb"; sha256 = "9cc055c96a53dc11e2d3ac6c22b99622cc0626c6d0ecf916827c741671eae204"; }
-{ url = "http://archive.ubuntu.com/ubuntu/pool/main/z/zlib/zlib1g-dev_1.2.3.3.dfsg-15ubuntu1_i386.deb"; sha256 = "e1041b3a4fff2c094f2364bc28d7624e50b4da38aab91f2a43f2e732ceb2b699"; }
 ]
